abc3226dd8253991127d5cd9e391ee325a677c5e
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2024-01-25 14:06:07 +0100

    Delete teams directory

--- teams/src/main/java/tsi/teams/repositories/EvenementRepo.java
+++
@@ -1,6 +0,0 @@
-package tsi.teams.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import tsi.teams.models.Evenement;
-public interface EvenementRepo extends CrudRepository<Evenement, Long> {
-}
